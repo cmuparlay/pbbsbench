ac9161054a75abb9e7c7edcc1cacb5a0aaea0fa6
Magdalen Dobson <magdalen@aware.aladdin.cs.cmu.edu>
2021-03-05 22:50:12 -0500

    added sort

--- nearestNeighbors/cgalnew/neighbors.h
+++ nearestNeighbors/cgalnew/neighbors.h
@@ -8,6 +8,7 @@
 #include "parlay/parallel.h"
 #include "parlay/primitives.h"
 #include "common/get_time.h"
+#include "../octTree/oct_tree.h"
 
 int algorithm_version = 3; // just to interface with the timing code
 
@@ -19,8 +20,39 @@ using Neighbor_search = CGAL::Orthogonal_k_neighbor_search<Traits>;
 using Tree = Neighbor_search::Tree;
 using Point_with_distance = Neighbor_search::Point_with_transformed_distance;
 
+template<class vtx>
+struct sorter{
+  using o_tree = oct_tree<vtx>;
+  using box = typename o_tree::box;
+
+  parlay::sequence<vtx*> z_sort(parlay::sequence<vtx*> v, box b, double Delta){ 
+    using indexed_point = typename o_tree::indexed_point; 
+    size_t n = v.size();
+    parlay::sequence<indexed_point> points;
+    points = parlay::sequence<indexed_point>(n);
+    parlay::parallel_for(0, n, [&] (size_t i){
+      size_t p1 = o_tree::interleave_bits(v[i]->pt, b.first, Delta);
+      indexed_point i1 = std::make_pair(p1, v[i]);
+      points[i] = i1; 
+    });
+    auto less = [&] (indexed_point a, indexed_point b){
+      return a.first < b.first;
+    };
+    auto x = parlay::sort(points, less);
+    parlay::sequence<vtx*> v3; 
+    v3 = parlay::sequence<vtx*>(n);
+    parlay::parallel_for(0, n, [&] (size_t i){
+      v3[i] = x[i].second; 
+    });
+    return v3; 
+  }
+
+};
+
 template <int max_k, class vtx>
 void ANN(parlay::sequence<vtx*> &v, int k) {
+  using o_tree = oct_tree<vtx>;
+  using box = typename o_tree::box;
   timer t("ANN",true);
   
   const unsigned int N = v.size();
@@ -32,8 +64,25 @@ void ANN(parlay::sequence<vtx*> &v, int k) {
   //   points.push_back (Point_3(pt[0],pt[1],pt[2]));
   // }
 
-  auto points = parlay::tabulate(N, [&] (size_t i) {
-				      auto pt = v[i]->pt;
+  auto minmax = [&] (box x, box y) {
+    return box(x.first.minCoords(y.first),
+         x.second.maxCoords(y.second));};
+  
+  // uses a delayed sequence to avoid making a copy
+  auto pts = parlay::delayed_seq<box>(N, [&] (size_t i) {
+      return box(v[i]->pt, v[i]->pt);});
+  box identity = pts[0];
+  box b = parlay::reduce(pts, parlay::make_monoid(minmax,identity));
+
+  double Delta = 0;
+  for (int i = 0; i < N; i++) 
+    Delta = std::max(Delta, b.second[i] - b.first[i]); 
+
+  sorter<vtx> S;
+  parlay::sequence<vtx*> v1 = S.z_sort(v, b, Delta);
+
+  auto points = parlay::tabulate(N, [&] (size_t i) {    
+				      auto pt = v1[i]->pt;
 				      return Point_3(pt[0],pt[1],pt[2]);});
   t.next("convert data");
 
