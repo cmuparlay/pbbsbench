34bb59436d34fc9067eb6220babb9a8ee30976d0
Daniel Anderson <dlanders@cs.cmu.edu>
2021-08-04 13:38:32 -0400

    Add verbose option to rayCast implementation

--- rayCast/bench/ray.h
+++ rayCast/bench/ray.h
@@ -6,4 +6,4 @@ using point = point3d<double>;
 using index_t = int; // if more than 2 billion rays, then should be long
 
 parlay::sequence<index_t> rayCast(triangles<point> const &Tri,
-				  parlay::sequence<ray<point>> const &rays);
+				  parlay::sequence<ray<point>> const &rays, bool verbose);
