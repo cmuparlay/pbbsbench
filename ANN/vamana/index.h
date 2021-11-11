a67cfd9716d3a219e0ddce2503df1beb566709c5
Magdalen Dobson <magdalen@aware.aladdin.cs.cmu.edu>
2021-11-11 17:06:40 -0500

    added beam search

--- ANN/vamana/index.h
+++ ANN/vamana/index.h
@@ -33,6 +33,7 @@ struct knn_index{
 	int maxDeg;
 	int beamSize;
 	int k; 
+	fvec_point* medoid; 
 	using slice_fvec = decltype(make_slice(parlay::sequence<fvec_point*>()));
 
 	knn_index(int md, int bs, int kk) : maxDeg(md), beamSize(bs), k(kk) {}
@@ -104,25 +105,89 @@ struct knn_index{
 		}
 	}
 
-
 	//computes the centroid and then assigns the approx medoid as the point in v
 	//closest to the centroid
-	fvec_point* find_approx_medoid(parlay::sequence<fvec_point*> v){
+	void find_approx_medoid(parlay::sequence<fvec_point*> v){
 		size_t n = v.size();
 		int d = ((v[0]->coordinates).size())/4;
 		parlay::sequence<float> centroid = centroid_helper(parlay::make_slice(v), d);
 		fvec_point centroidp = typename fvec_point::fvec_point();
 		centroidp.coordinates = parlay::make_slice(centroid);
-		fvec_point* medoid = medoid_helper(&centroidp, parlay::make_slice(v), d);
-		return medoid; 
+		medoid = medoid_helper(&centroidp, parlay::make_slice(v), d); 
 	}
 
-	parlay::sequence<int> beam_collect(){
-
+	//returns true if F \setminus V = emptyset 
+	bool intersect_nonempty(std::set<int> V, parlay::sequence<int> F){
+		int Fsize = F.size();
+		for(int i=0; i<Fsize; i++){
+			if(V.find(F[i]) == V.end()) return true; 
+		}
+		return false;
 	}
+
+	int id_next(std::set<int> V, parlay::sequence<int> F){
+		int fsize = F.size();
+		for(int i=0; i<fsize; i++){
+			if(V.find(F[i]) == V.end()) return F[i];
+		}
+		return 0;
+	}
+
+
+	//actually should redo this using sets, just also need a sorted list for the frontier
+	//so frontier has a sorted list AND a set, while visited has just a set
+	std::pair<std::set<int>, std::set<int>> beam_search(fvec_point* p, parlay::sequence<fvec_point*> v){
+		std::set<int> visited;
+		std::set<int> frontier; 
+		parlay::sequence<int> sortedFrontier = parlay::sequence<int>();
+		sortedFrontier.push_back(medoid->id);
+		frontier.insert(medoid->id);
+		while(intersect_nonempty(visited, sortedFrontier)){
+			//identify the node we are visiting
+			int currentIndex = id_next(visited, sortedFrontier);
+			fvec_point* current = v[currentIndex]; 
+			parlay::sequence<int> outnbh = current->out_nbh;
+			for(int i=0; i++; i<(outnbh.size())){
+				if(frontier.find(outnbh[i]) != frontier.end()){
+					auto less = [&] (int a){
+						return distance(v[a], p) < distance(v[outnbh[i]], p);
+						// return a<outnbh[i]; 
+					};
+					int insertion_point = parlay::internal::binary_search(parlay::make_slice(sortedFrontier), less);
+					// sortedFrontier.begin()+insertion_point; 
+					const int to_insert = outnbh[i];
+					sortedFrontier.insert(sortedFrontier.begin()+insertion_point, to_insert);
+					frontier.insert(outnbh[i]);
+				}
+			}
+			//remove nodes from frontier if too large
+			if(sortedFrontier.size() > beamSize){ //beamSize is a global var taken from command line
+				for(int i=sortedFrontier.size(); i>beamSize; i--){
+					int to_del = sortedFrontier[i];
+					sortedFrontier.pop_back();
+					frontier.erase(to_del);
+				}
+			}
+			//add the node to the visited list
+			visited.insert(current->id);
+		}
+		return std::make_pair(frontier, visited);
+	}
+
+	// void robustPrune(fvec_point* p, std::set<int> candidates){
+	// 	//transform the candidate set to a sequence, add the out_nbhs of p, and sort
+	// 	for(int i=0; i<(p->out_nbh.size()); i++){
+	// 		candidates.insert(p->out_nbh[i]);
+	// 	}
+	// 	parlay::sequence<int> sortedCandidates = parlay::sequence<int>(candidates.size());
+	// 	for(int i=0; i<candidates.size(); i++){ //need to use the set iterator
+
+	// 	}
+	// }
 
 	void build_index(parlay::sequence<fvec_point*> v){
 		random_index(v);
 		find_approx_medoid(v);
+		beam_search(v[0], v);
 	}
 };
