6ed311d12be9a57e93203864509463fb829cc1b7
Magdalen Dobson <magdalen@aware.aladdin.cs.cmu.edu>
2022-03-26 12:23:03 -0400

    more changes to allocator

--- benchmarks/ANN/utils/seq_allocator.h
+++ benchmarks/ANN/utils/seq_allocator.h
@@ -41,22 +41,23 @@ inline std::vector<size_t> default_sizes() {
 }
 
 
+namespace fine{
+	extern inline parlay::pool_allocator& get_fine_default_allocator() {
+	  static parlay::pool_allocator fine_default_allocator(default_sizes());
+	  return fine_default_allocator;
+	}
 
-extern inline parlay::pool_allocator& get_fine_default_allocator() {
-  static parlay::pool_allocator fine_default_allocator(default_sizes());
-  return fine_default_allocator;
-}
 
+	// pair of total currently used space, and total unused space the allocator has in reserve
+	extern inline std::pair<size_t,size_t> memory_usage() {
+	  return get_fine_default_allocator().stats();
+	}
 
-// pair of total currently used space, and total unused space the allocator has in reserve
-extern inline std::pair<size_t,size_t> memory_usage() {
-  return get_fine_default_allocator().stats();
-}
-
-// pair of total currently used space, and total unused space the allocator has in reserve
-extern inline void memory_clear() {
-  return get_fine_default_allocator().clear();
-}
+	// pair of total currently used space, and total unused space the allocator has in reserve
+	extern inline void memory_clear() {
+	  return get_fine_default_allocator().clear();
+	}
+} //namespace fine
   
 
 // ****************************************
@@ -70,14 +71,47 @@ template <typename T>
 struct fine_allocator {
   using value_type = T;
   T* allocate(size_t n) {
-    return (T*) get_fine_default_allocator().allocate(n * sizeof(T));
+    return (T*) fine::get_fine_default_allocator().allocate(n * sizeof(T));
   }
   void deallocate(T* ptr, size_t n) {
-    get_fine_default_allocator().deallocate((void*) ptr, n * sizeof(T));
+    fine::get_fine_default_allocator().deallocate((void*) ptr, n * sizeof(T));
   }
 
   constexpr fine_allocator() = default;
   template <class U> constexpr fine_allocator(const allocator<U>&) noexcept { }
 };
+
+template <class T, class U>
+bool operator==(const fine_allocator<T>&, const fine_allocator<U>&) { return true; }
+template <class T, class U>
+bool operator!=(const fine_allocator<T>&, const fine_allocator<U>&) { return false; }
+
+constexpr size_t size_offset = 1; // in size_t sized words
+
+// needs to be at least size_offset * sizeof(size_t)
+inline size_t header_size(size_t n) { // in bytes
+  return (n >= 1024) ? 64 : (n & 15) ? 8 : (n & 63) ? 16 : 64;
+}
+
+// allocates and tags with a header (8, 16 or 64 bytes) that contains the size
+extern inline void* p_malloc(size_t n) {
+  size_t hsize = header_size(n);
+  void* ptr = fine::get_fine_default_allocator().allocate(n + hsize);
+  void* r = (void*) (((char*) ptr) + hsize);
+  *(((size_t*) r) - size_offset) = n; // puts size in header
+  return r;
+}
+
+// reads the size, offsets the header and frees
+extern inline void p_free(void* ptr) {
+  size_t n = *(((size_t*) ptr) - size_offset);
+  size_t hsize = header_size(n);
+  if (hsize > (1ull << 48)) {
+    std::cout << "corrupted header in my_free" << std::endl;
+    throw std::bad_alloc();
+  }
+  fine::get_fine_default_allocator().deallocate((void*) (((char*) ptr) - hsize),
+                                               n + hsize);
+}
 
 #endif
