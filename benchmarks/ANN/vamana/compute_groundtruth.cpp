ae470fbb64e5551c7ec0b71023f8c7a841d30dd5
Magdalen Dobson <magdalen@aware.aladdin.cs.cmu.edu>
2023-01-30 14:49:15 -0500

    groundtruth

---
+++ benchmarks/ANN/vamana/compute_groundtruth.cpp
@@ -0,0 +1,183 @@
+#include <iostream>
+#include <algorithm>
+#include "parlay/parallel.h"
+#include "parlay/primitives.h"
+#include "parlay/io.h"
+#include "../utils/types.h"
+#include "../utils/NSGDist.h"
+#include "../utils/parse_files.h"
+
+using pid = std::pair<int, float>;
+
+
+template<typename T>
+void compute_groundtruth(parlay::sequence<Tvec_point<T>> &B, parlay::sequence<Tvec_point<T>> &Q, int k, 
+    parlay::sequence<parlay::sequence<pid>> &result){
+    
+    unsigned d = (B[0].coordinates).size();
+    parlay::parallel_for(0, Q.size(), [&] (size_t i){
+        float topdist;
+        int toppos;
+        parlay::sequence<pid> topk;
+        for(size_t j=0; j<B.size(); j++){
+            float dist = distance((Q[i].coordinates).begin(), (B[j].coordinates).begin(), d);
+            if(topk.size() < k){
+                if(dist > topdist){
+                    topdist = dist;
+                    toppos = topk.size();
+                }
+                topk.push_back(std::make_pair((int) j, dist));
+                continue;
+            }
+            if(dist < topdist){
+                float new_topdist=0;
+                int new_toppos=0;
+                topk[toppos] = std::make_pair((int) j, dist);
+                for(size_t l=0; l<topk.size(); l++){
+                    if(topk[l].second > new_topdist){
+                        new_topdist = topk[l].second;
+                        new_toppos = (int) l;
+                    }
+                }
+                topdist = new_topdist;
+                toppos = new_toppos;
+            }
+        }
+        result[i] = topk;
+    });
+}
+
+void write_ivecs(parlay::sequence<parlay::sequence<pid>> &result, const std::string outFile, int k){
+    std::cout << "Writing file with dimension " << result[0].size() << std::endl;
+    std::cout << "File contains groundtruth for " << result.size() << " data points" << std::endl;
+
+    auto less = [&] (pid a, pid b) {return a.second < b.second;};
+
+    size_t n = result.size();
+    auto vects = parlay::tabulate(result.size(), [&] (size_t i){
+        parlay::sequence<int> data;
+        data.push_back(k);
+
+        auto sorted = parlay::sort(result[i], less);
+        for(int j=0; j<k; j++){
+          data.push_back(sorted[j].first);
+        }
+        return data;
+    });
+
+    parlay::sequence<int> to_write = parlay::flatten(vects);
+
+    auto data = to_write.begin();
+    std::ofstream writer;
+    writer.open(outFile, std::ios::binary | std::ios::out);
+    writer.write((char *) data, n * (k+1) * sizeof(int));
+    writer.close();
+}
+
+void write_ibin(parlay::sequence<parlay::sequence<pid>> &result, const std::string outFile, int k){
+    std::cout << "Writing file with dimension " << result[0].size() << std::endl;
+    std::cout << "File contains groundtruth for " << result.size() << " data points" << std::endl;
+
+    auto less = [&] (pid a, pid b) {return a.second < b.second;};
+
+    size_t n = result.size();
+    auto vects = parlay::tabulate(result.size(), [&] (size_t i){
+        parlay::sequence<int> data;
+
+        auto sorted = parlay::sort(result[i], less);
+        for(int j=0; j<k; j++){
+          data.push_back(sorted[j].first);
+        }
+        return data;
+    });
+
+    parlay::sequence<int> to_write = parlay::flatten(vects);
+
+    auto data = to_write.begin();
+    std::ofstream writer;
+    writer.open(outFile, std::ios::binary | std::ios::out);
+    writer.write((char *) data, n * k * sizeof(int));
+    writer.close();
+}
+
+
+int main(int argc, char* argv[]) {
+  if (argc != 7) {
+    std::cout << "usage: compute_groundtruth <base> <query> <filetype> <vectype> <k> <oFile>" << std::endl;
+    return 1;
+  }
+  int k = std::atoi(argv[5]);
+  std::string ft = std::string(argv[3]);
+  std::string tp = std::string(argv[4]);
+
+  std::cout << "Computing the " << k << " nearest neighbors" << std::endl;
+  if((ft != "bin") && (ft != "vec")){
+    std::cout << "Error: file type not specified correctly, specify bin or vec" << std::endl;
+    abort();
+  }
+
+  if((tp != "uint8") && (tp != "int8") && (tp != "float")){
+    std::cout << "Error: vector type not specified correctly, specify int8, uint8, or float" << std::endl;
+    abort();
+  }
+
+  if((ft == "vec") && (tp == "int8")){
+    std::cout << "Error: incompatible file and vector types" << std::endl;
+    abort();
+  }
+
+  int maxDeg = 0;
+
+  parlay::sequence<parlay::sequence<pid>> result;
+
+  if(ft == "vec"){
+    if(tp == "float"){
+      std::cout << "Detected float coordinates" << std::endl;
+      auto [md, B] = parse_fvecs(argv[1], NULL, maxDeg);
+      auto [fd, Q] = parse_fvecs(argv[2], NULL, maxDeg);
+      std::cout << "Base file size " << B.size() << std::endl;
+      std::cout << "Query file size " << Q.size() << std::endl;
+      result = parlay::sequence<parlay::sequence<pid>>(Q.size());
+      compute_groundtruth<float>(B, Q, k, result);
+    }else if(tp == "uint8"){
+      std::cout << "Detected uint8 coordinates" << std::endl;
+      auto [md, B] = parse_bvecs(argv[1], NULL, maxDeg);
+      auto [fd, Q] = parse_bvecs(argv[2], NULL, maxDeg);
+      std::cout << "Base file size " << B.size() << std::endl;
+      std::cout << "Query file size " << Q.size() << std::endl;
+      result = parlay::sequence<parlay::sequence<pid>>(Q.size());
+      compute_groundtruth<uint8_t>(B, Q, k, result);
+    }
+    write_ivecs(result, std::string(argv[4]), k);
+  } else if(ft == "bin"){
+    if(tp == "float"){
+      std::cout << "Detected float coordinates" << std::endl;
+      auto [md, B] = parse_fbin(argv[1], NULL, maxDeg);
+      auto [fd, Q] = parse_fbin(argv[2], NULL, maxDeg);
+      std::cout << "Base file size " << B.size() << std::endl;
+      std::cout << "Query file size " << Q.size() << std::endl;
+      result = parlay::sequence<parlay::sequence<pid>>(Q.size());
+      compute_groundtruth<float>(B, Q, k, result);
+    }else if(tp == "uint8"){
+      std::cout << "Detected uint8 coordinates" << std::endl;
+      auto [md, B] = parse_uint8bin(argv[1], NULL, maxDeg);
+      auto [fd, Q] = parse_uint8bin(argv[2], NULL, maxDeg);
+      std::cout << "Base file size " << B.size() << std::endl;
+      std::cout << "Query file size " << Q.size() << std::endl;
+      result = parlay::sequence<parlay::sequence<pid>>(Q.size());
+      compute_groundtruth<uint8_t>(B, Q, k, result);
+    }else if(tp == "int8"){
+      std::cout << "Detected int8 coordinates" << std::endl;
+      auto [md, B] = parse_int8bin(argv[1], NULL, maxDeg);
+      auto [fd, Q] = parse_int8bin(argv[2], NULL, maxDeg);
+      std::cout << "Base file size " << B.size() << std::endl;
+      std::cout << "Query file size " << Q.size() << std::endl;
+      result = parlay::sequence<parlay::sequence<pid>>(Q.size());
+      compute_groundtruth<int8_t>(B, Q, k, result);
+    }
+  }
+
+  
+
+  return 0;
+}
