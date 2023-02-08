cae239cf4e825d143b2c3f5edace694a86027602
Ubuntu <azureuser@ANS1.1tndt1sllxmubnyf32xqp4ovzc.bx.internal.cloudapp.net>
2023-02-08 22:37:28 +0000

    script for testing numa on large machine

--- benchmarks/ANN/scripts/test_HCNNG.sh
+++ benchmarks/ANN/scripts/test_HCNNG.sh
@@ -19,4 +19,4 @@ PARLAY_NUM_THREADS=1 numactl -i all ./neighbors -a 1000 -R 3 -L 10 -b 1 -o $BG/1
 PARLAY_NUM_THREADS=2 numactl -i all ./neighbors -a 1000 -R 3 -L 10 -b 1 -o $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $BG/test_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
 PARLAY_NUM_THREADS=48 numactl -i all  ./neighbors -a 1000 -R 3 -L 10 -b 1 -o $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $BG/test_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
 PARLAY_NUM_THREADS=96 numactl -i all  ./neighbors -a 1000 -R 3 -L 10 -b 1 -o $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $BG/test_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
-numactl -i all ./neighbors -a 1000 -R 3 -L 30 -b 1 -o $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $BG/test_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
+numactl -i all ./neighbors -a 1000 -R 3 -L 10 -b 1 -o $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $BG/test_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
