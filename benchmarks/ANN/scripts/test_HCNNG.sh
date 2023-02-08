cdf3ad8d45ed24e2aeed3f8f3454c97b5ded675c
Ubuntu <azureuser@ANS1.1tndt1sllxmubnyf32xqp4ovzc.bx.internal.cloudapp.net>
2023-02-08 22:30:55 +0000

    script for testing numa on large machine

--- benchmarks/ANN/scripts/test_HCNNG.sh
+++ benchmarks/ANN/scripts/test_HCNNG.sh
@@ -8,15 +8,15 @@ BP=$P/bigann
 BG=$G/bigann
 
 #test with varying num_threads
-PARLAY_NUM_THREADS=1 ./neighbors -a 1000 -R 3 -L 30 -b 1 -o $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $BG/HCNNG_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
-PARLAY_NUM_THREADS=2 ./neighbors -a 1000 -R 3 -L 30 -b 1 -o $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $BG/HCNNG_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
-PARLAY_NUM_THREADS=48 ./neighbors -a 1000 -R 3 -L 30 -b 1 -o $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $BG/HCNNG_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
-PARLAY_NUM_THREADS=96 ./neighbors -a 1000 -R 3 -L 30 -b 1 -o $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $BG/HCNNG_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
-./neighbors -a 1000 -R 3 -L 30 -b 1 -o $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $BG/HCNNG_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
+PARLAY_NUM_THREADS=1 ./neighbors -a 1000 -R 3 -L 10 -b 1 -o $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $BG/test_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
+PARLAY_NUM_THREADS=2 ./neighbors -a 1000 -R 3 -L 10 -b 1 -o $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $BG/test_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
+PARLAY_NUM_THREADS=48 ./neighbors -a 1000 -R 3 -L 10 -b 1 -o $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $BG/test_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
+PARLAY_NUM_THREADS=96 ./neighbors -a 1000 -R 3 -L 10 -b 1 -o $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $BG/test_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
+./neighbors -a 1000 -R 3 -L 30 -b 1 -o $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $BG/test_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
 
 #test with varying num_threads and numa
-PARLAY_NUM_THREADS=1 numactl -i all ./neighbors -a 1000 -R 3 -L 30 -b 1 -o $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $BG/HCNNG_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
-PARLAY_NUM_THREADS=2 numactl -i all ./neighbors -a 1000 -R 3 -L 30 -b 1 -o $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $BG/HCNNG_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
-PARLAY_NUM_THREADS=48 numactl -i all  ./neighbors -a 1000 -R 3 -L 30 -b 1 -o $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $BG/HCNNG_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
-PARLAY_NUM_THREADS=96 numactl -i all  ./neighbors -a 1000 -R 3 -L 30 -b 1 -o $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $BG/HCNNG_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
-numactl -i all ./neighbors -a 1000 -R 3 -L 30 -b 1 -o $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $BG/HCNNG_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
+PARLAY_NUM_THREADS=1 numactl -i all ./neighbors -a 1000 -R 3 -L 10 -b 1 -o $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $BG/test_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
+PARLAY_NUM_THREADS=2 numactl -i all ./neighbors -a 1000 -R 3 -L 10 -b 1 -o $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $BG/test_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
+PARLAY_NUM_THREADS=48 numactl -i all  ./neighbors -a 1000 -R 3 -L 10 -b 1 -o $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $BG/test_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
+PARLAY_NUM_THREADS=96 numactl -i all  ./neighbors -a 1000 -R 3 -L 10 -b 1 -o $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $BG/test_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
+numactl -i all ./neighbors -a 1000 -R 3 -L 30 -b 1 -o $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $BG/test_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
