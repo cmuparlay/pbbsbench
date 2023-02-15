50c900bdb2a39ef731801c3c8a73980ca5b58d36
Magdalen Dobson <magdalen@aware.aladdin.cs.cmu.edu>
2023-02-15 16:53:18 -0500

    updated scripts

--- benchmarks/rangeSearch/scripts/vamana.sh
+++ benchmarks/rangeSearch/scripts/vamana.sh
@@ -13,5 +13,4 @@ R=/ssd1/results/FB_ssnpp
 ./range -a 1.2 -R 128 -L 256 -q $P/FB_ssnpp_public_queries.u8bin -o $R/100M_128_256 -c $P/ssnpp-100M -res $R/vamana_res.csv $P/FB_ssnpp_database.u8bin.crop_nb_100000000
 ./range -a 1.2 -R 150 -L 400 -q $P/FB_ssnpp_public_queries.u8bin -o $R/100M_150_400 -c $P/ssnpp-100M -res $R/vamana_res.csv $P/FB_ssnpp_database.u8bin.crop_nb_100000000
 
-./range -a 1.2 -R 128 -L 256 -q $P/FB_ssnpp_public_queries.u8bin -o $R/1B_128_256 -c $P/FB_ssnpp_public_queries_1B_GT.rangeres -res $R/vamana_res.csv $P/FB_ssnpp_database.u8bin
 ./range -a 1.2 -R 150 -L 400 -q $P/FB_ssnpp_public_queries.u8bin -o $R/1B_150_400 -c $P/FB_ssnpp_public_queries_1B_GT.rangeres -res $R/vamana_res.csv $P/FB_ssnpp_database.u8bin
