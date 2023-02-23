8e7acb0d4256ef188ed92afcd33634524a3351d1
Ubuntu <azureuser@ANS1.1tndt1sllxmubnyf32xqp4ovzc.bx.internal.cloudapp.net>
2023-02-23 19:42:52 +0000

    searching script for range search

---
+++ benchmarks/rangeSearch/scripts/search_all.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+cd ~/pbbsbench/benchmarks/rangeSearch/vamana
+
+make clean all
+P=/ssd1/data/FB_ssnpp
+R=/ssd1/results/FB_ssnpp
+./range -a 1.2 -R 128 -L 256 -q $P/FB_ssnpp_public_queries.u8bin -g $R/1M_128_256 -c $P/ssnpp-1M -res $R/vamana_res.csv $P/FB_ssnpp_database.u8bin.crop_nb_1000000
+./range -a 1.2 -R 150 -L 400 -q $P/FB_ssnpp_public_queries.u8bin -g $R/1M_150_400 -c $P/ssnpp-1M -res $R/vamana_res.csv $P/FB_ssnpp_database.u8bin.crop_nb_1000000
+
+./range -a 1.2 -R 128 -L 256 -q $P/FB_ssnpp_public_queries.u8bin -g $R/10M_128_256 -c $P/ssnpp-10M -res $R/vamana_res.csv $P/FB_ssnpp_database.u8bin.crop_nb_10000000
+./range -a 1.2 -R 150 -L 400 -q $P/FB_ssnpp_public_queries.u8bin -g $R/10M_150_400 -c $P/ssnpp-10M -res $R/vamana_res.csv $P/FB_ssnpp_database.u8bin.crop_nb_10000000
+
+./range -a 1.2 -R 128 -L 256 -q $P/FB_ssnpp_public_queries.u8bin -g $R/100M_128_256 -c $P/ssnpp-100M -res $R/vamana_res.csv $P/FB_ssnpp_database.u8bin.crop_nb_100000000
+./range -a 1.2 -R 150 -L 400 -q $P/FB_ssnpp_public_queries.u8bin -g $R/100M_150_400 -c $P/ssnpp-100M -res $R/vamana_res.csv $P/FB_ssnpp_database.u8bin.crop_nb_100000000
+
+./range -a 1.2 -R 150 -L 400 -q $P/FB_ssnpp_public_queries.u8bin -g $R/1B_150_400 -c $P/FB_ssnpp_public_queries_1B_GT.rangeres -res $R/vamana_res.csv $P/FB_ssnpp_database.u8bin
+
+cd ~/pbbsbench/benchmarks/rangeSearch/HCNNG
+
+make clean all
+Q=/ssd1/data/FB_ssnpp
+S=/ssd1/results/FB_ssnpp
+./range -a 1000 -R 3 -L 30 -b 1 -q $Q/FB_ssnpp_public_queries.u8bin -g $S/1M_3_30 -c $Q/ssnpp-1M -res $S/hcnng_res.csv $Q/FB_ssnpp_database.u8bin.crop_nb_1000000
+./range -a 1000 -R 3 -L 50 -b 1 -q $Q/FB_ssnpp_public_queries.u8bin -g $S/1M_3_50 -c $Q/ssnpp-1M -res $S/hcnng_res.csv $Q/FB_ssnpp_database.u8bin.crop_nb_1000000
+
+./range -a 1000 -R 3 -L 30 -b 1 -q $Q/FB_ssnpp_public_queries.u8bin -g $S/10M_3_30 -c $Q/ssnpp-10M -res $S/hcnng_res.csv $Q/FB_ssnpp_database.u8bin.crop_nb_10000000
+./range -a 1000 -R 3 -L 50 -b 1 -q $Q/FB_ssnpp_public_queries.u8bin -g $S/10M_3_50 -c $Q/ssnpp-10M -res $S/hcnng_res.csv $Q/FB_ssnpp_database.u8bin.crop_nb_10000000
+
+./range -a 1000 -R 3 -L 30 -b 1 -q $Q/FB_ssnpp_public_queries.u8bin -g $S/100M_3_30 -c $Q/ssnpp-100M -res $S/hcnng_res.csv $Q/FB_ssnpp_database.u8bin.crop_nb_100000000
+./range -a 1000 -R 3 -L 50 -b 1 -q $Q/FB_ssnpp_public_queries.u8bin -g $S/100M_3_50 -c $Q/ssnpp-100M -res $S/hcnng_res.csv $Q/FB_ssnpp_database.u8bin.crop_nb_100000000
+
+./range -a 1000 -R 3 -L 50 -b 1 -q $Q/FB_ssnpp_public_queries.u8bin -g $S/1B_3_50 -c $Q/FB_ssnpp_public_queries_1B_GT.rangeres -res $S/hcnng_res.csv $Q/FB_ssnpp_database.u8bin
+
+cd ~/pbbsbench/benchmarks/rangeSearch/pyNNDescent
+
+make clean all
+T=/ssd1/data/FB_ssnpp
+U=/ssd1/results/FB_ssnpp
+./range -R 60 -L 1000 -a 10 -d 1.2 -b 2 -q $T/FB_ssnpp_public_queries.u8bin -g $U/1M_60 -c $T/ssnpp-1M -res $U/pynn_res.csv $T/FB_ssnpp_database.u8bin.crop_nb_1000000
+./range -R 90 -L 1000 -a 10 -d 1.2 -b 2 -q $T/FB_ssnpp_public_queries.u8bin -g $U/1M_90 -c $T/ssnpp-1M -res $U/pynn_res.csv $T/FB_ssnpp_database.u8bin.crop_nb_1000000
+
+./range -R 60 -L 1000 -a 10 -d 1.2 -b 2 -q $T/FB_ssnpp_public_queries.u8bin -g $U/10M_60 -c $T/ssnpp-10M -res $U/pynn_res.csv $T/FB_ssnpp_database.u8bin.crop_nb_10000000
+./range -R 90 -L 1000 -a 10 -d 1.2 -b 2 -q $T/FB_ssnpp_public_queries.u8bin -g $U/10M_90 -c $T/ssnpp-10M -res $U/pynn_res.csv $T/FB_ssnpp_database.u8bin.crop_nb_10000000
+
+./range -R 60 -L 1000 -a 10 -d 1.2 -b 2 -q $T/FB_ssnpp_public_queries.u8bin -g $U/100M_60 -c $T/ssnpp-100M -res $U/pynn_res.csv $T/FB_ssnpp_database.u8bin.crop_nb_100000000
+./range -R 90 -L 1000 -a 10 -d 1.2 -b 2 -q $T/FB_ssnpp_public_queries.u8bin -g $U/100M_90 -c $T/ssnpp-100M -res $U/pynn_res.csv $T/FB_ssnpp_database.u8bin.crop_nb_100000000
