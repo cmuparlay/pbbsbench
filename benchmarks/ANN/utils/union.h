5c013943f3f96d6bcd26a169343df1b2d7b9d0a3
Magdalen Dobson <magdalen@aware.aladdin.cs.cmu.edu>
2023-03-12 21:40:53 -0400

    changes to random clustering

--- benchmarks/ANN/utils/union.h
+++ benchmarks/ANN/utils/union.h
@@ -26,6 +26,69 @@
 #define UNION
 
 using pid = std::pair<int, float>;
+using nbh = std::pair<pid, bool>;
+
+// takes in two sorted sequences and returns a sorted union
+std::pair<parlay::sequence<pid>, bool> seq_union_bounded(parlay::sequence<pid> &P,
+                                parlay::sequence<pid>& Q, 
+                                int K) {
+  auto less = [&](pid a, pid b) { return a.second < b.second; };
+  pid* first1 = P.begin();
+  pid* last1 = P.end();
+  pid* first2 = Q.begin();
+  pid* last2 = Q.end();
+  bool changed = false;
+  parlay::sequence<pid> result = parlay::sequence<pid>();
+  result.reserve(K);
+  int count=0;
+  while (true && count<K) {
+    if (first1 == last1) {
+      while (first2 != last2 && count < K) {
+        changed=true;
+        result.push_back(*first2);
+        count++;
+        ++first2;
+      }
+      return std::make_pair(result, changed);
+    } else if (first2 == last2) {
+      while (first1 != last1 && count < K) {
+        result.push_back(*first1);
+        count++;
+        ++first1;
+      }
+      return std::make_pair(result, changed);
+    }
+    if (less(*first1, *first2)) {
+      result.push_back(*first1);
+      count++;
+      ++first1;
+    } else if (less(*first2, *first1)) {
+      result.push_back(*first2);
+      changed=true;
+      count++;
+      ++first2;
+    } else {
+      if (first1->first == first2->first) {
+        result.push_back(*first1);
+        count++;
+        ++first1;
+        ++first2;
+      } else {
+        result.push_back(*first1);
+        count++;
+        if(count==K) break;
+        else{
+          result.push_back(*first2);
+          changed=true;
+          count++;
+          ++first1;
+          ++first2;
+        }
+      }
+    }
+  }
+  return std::make_pair(result, changed);
+}
 
 // takes in two sorted sequences and returns a sorted union
 template <typename Seq>
