18d67bcc1d8c903dc4f95945b4210dde40580aca
Ubuntu <azureuser@ANS1.1tndt1sllxmubnyf32xqp4ovzc.bx.internal.cloudapp.net>
2023-03-17 17:25:56 +0000

    small changes

--- benchmarks/ANN/scripts/pynndescent.sh
+++ benchmarks/ANN/scripts/pynndescent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~/pbbsbench/benchmarks/ANN/pyNNDescent
-make 
+make clean all
 
 P=/ssd1/data
 G=/ssd1/results
@@ -21,7 +21,7 @@ SG=$G/MSSPACEV1B
 # #TEXT2IMAGE: two settings
 TP=$P/text2image1B
 TG=$G/text2image1B
-./neighbors -R 60 -L 100 -a 10 -d .9 -o $TG/1M_pynn_60 -q $TP/query.public.100K.fbin -c $TP/text2image-1M -res $TG/pynn_res3.csv -f bin -t float -D 1 $TP/base.1B.fbin.crop_nb_1000000
-./neighbors -R 60 -L 100 -a 10 -d .9 -o $TG/10M_pynn_60 -q $TP/query.public.100K.fbin -c $TP/text2image-10M -res $TG/pynn_res3.csv -f bin -t float -D 1 $TP/base.1B.fbin.crop_nb_10000000
-./neighbors -R 60 -L 100 -a 10 -d .9 -o $TG/100M_pynn_60 -q $TP/query.public.100K.fbin -c $TP/text2image-100M -res $TG/pynn_res3.csv -f bin -t float -D 1 $TP/base.1B.fbin.crop_nb_100000000
+./neighbors -R 60 -L 100 -a 10 -d .9 -o $TG/1M_pynn_60 -q $TP/query.public.100K.fbin -c $TP/text2image-1M -res $TG/pynn_res.csv -f bin -t float -D 1 $TP/base.1B.fbin.crop_nb_1000000
+./neighbors -R 60 -L 100 -a 10 -d .9 -o $TG/10M_pynn_60 -q $TP/query.public.100K.fbin -c $TP/text2image-10M -res $TG/pynn_res.csv -f bin -t float -D 1 $TP/base.1B.fbin.crop_nb_10000000
+./neighbors -R 60 -L 100 -a 10 -d .9 -o $TG/100M_pynn_60 -q $TP/query.public.100K.fbin -c $TP/text2image-100M -res $TG/pynn_res.csv -f bin -t float -D 1 $TP/base.1B.fbin.crop_nb_100000000
 
