170602dea8a1a863008e72d7286d10355c3df52f
Ubuntu <azureuser@ANS1.1tndt1sllxmubnyf32xqp4ovzc.bx.internal.cloudapp.net>
2023-03-23 15:29:13 +0000

    changes to scripting

--- benchmarks/ANN/scripts/all_experiments.sh
+++ benchmarks/ANN/scripts/all_experiments.sh
@@ -1,27 +1,15 @@
 #!/bin/bash
-#TODO separate based on machine?
-#minimal preparation: est 10 mins
-cd ~/pbbsbench/benchmarks/ANN/scripts
-bash prepare_datasets.sh
-#pynndescent on all datasets up to 100M: est 12 hours
-cd ~/pbbsbench/benchmarks/ANN/scripts
-bash pynndescent.sh
-#FAISS with better strings for all datasets: est 16 hours
-cd ~/big-ann-benchmarks
-rm results
-mkdir /ssd1/results/FAISSresults2
-ln -s /ssd1/results/FAISSresults2 results
-cd ~/pbbsbench/benchmarks/ANN/scripts/FAISS
-bash run2.sh
-#HCNNG: mostly queries, one billion scale build: est 24 h
-cd ~/pbbsbench/benchmarks/ANN/scripts
-bash hcnng.sh
-#range search: 1 billion scale build with HCNNG, plus searches: est 18 hours
-#TODO check whether HCNNG build went through
-cd ~/pbbsbench/benchmarks/rangeSearch/scripts
-bash search_all.sh
-#vamana: 2 billion scale builds, with low parameters: est 
+#missing queries
+mkdir /ssd1/results/piped_output
 cd ~/pbbsbench/benchmarks/ANN/scripts
 bash vamana.sh
+bash hcnng.sh
+bash pynndescent.sh
+cd ~/pbbsbench/benchmarks/rangeSearch/scripts
+bash search_all.sh
+#FAISS with better strings for all datasets: est 16 hours
+cd ~/pbbsbench/benchmarks/ANN/scripts/FAISS
+bash run2.sh
+
 
 
--- benchmarks/ANN/scripts/hcnng.sh
+++ benchmarks/ANN/scripts/hcnng.sh
@@ -4,26 +4,27 @@ make clean all
 
 P=/ssd1/data
 G=/ssd1/results
+O=/ssd1/results/piped_output
 #BIGANN: two settings
 BP=$P/bigann
 BG=$G/bigann
-./neighbors -a 1000 -R 3 -L 30 -b 1 -g $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $BG/HCNNG_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
-./neighbors -a 1000 -R 3 -L 30 -b 1 -g $BG/10M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-10M -res $BG/HCNNG_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_10000000
-./neighbors -a 1000 -R 3 -L 30 -b 1 -g $BG/100M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-100M -res $BG/HCNNG_res.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_100000000
-./neighbors -a 1000 -R 3 -L 30 -b 1 -g $BG/1B_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1B -res $BG/HCNNG_res.csv -f bin -t uint8 $BP/base.1B.u8bin
+./neighbors -a 1000 -R 3 -L 30 -b 1 -g $BG/1M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $O/bigann_HCNNG.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
+./neighbors -a 1000 -R 3 -L 30 -b 1 -g $BG/10M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-10M -res $O/bigann_HCNNG.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_10000000
+./neighbors -a 1000 -R 3 -L 30 -b 1 -g $BG/100M_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-100M -res $O/bigann_HCNNG.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_100000000
+./neighbors -a 1000 -R 3 -L 30 -b 1 -g $BG/1B_HCNNG_30 -q $BP/query.public.10K.u8bin -c $BP/bigann-1B -res $O/bigann_HCNNG.csv -f bin -t uint8 $BP/base.1B.u8bin
 
 # #MSSPACEV: two settings
 SP=$P/MSSPACEV1B
 SG=$G/MSSPACEV1B
-./neighbors -a 1000 -R 3 -L 50 -b 1 -g $SG/1M_HCNNG_50 -q $SP/query.i8bin -c $SP/msspacev-1M -res $SG/HCNNG_res.csv -f bin -t int8 $SP/spacev1b_base.i8bin.crop_nb_1000000
-./neighbors -a 1000 -R 3 -L 50 -b 1 -g $SG/10M_HCNNG_50 -q $SP/query.i8bin -c $SP/msspacev-10M -res $SG/HCNNG_res.csv -f bin -t int8 $SP/spacev1b_base.i8bin.crop_nb_10000000
-./neighbors -a 1000 -R 3 -L 50 -b 1 -g $SG/100M_HCNNG_50 -q $SP/query.i8bin -c $SP/msspacev-100M -res $SG/HCNNG_res.csv -f bin -t int8 $SP/spacev1b_base.i8bin.crop_nb_100000000
-./neighbors -a 1000 -R 3 -L 50 -b 1 -g $SG/1B_HCNNG_50 -q $SP/query.i8bin -c $SP/msspacev-1B -res $SG/HCNNG_res.csv -f bin -t int8 $SP/spacev1b_base.i8bin
+./neighbors -a 1000 -R 3 -L 50 -b 1 -g $SG/1M_HCNNG_50 -q $SP/query.i8bin -c $SP/msspacev-1M -res $O/spacev_HCNNG.csv -f bin -t int8 $SP/spacev1b_base.i8bin.crop_nb_1000000
+./neighbors -a 1000 -R 3 -L 50 -b 1 -g $SG/10M_HCNNG_50 -q $SP/query.i8bin -c $SP/msspacev-10M -res $O/spacev_HCNNG.csv -f bin -t int8 $SP/spacev1b_base.i8bin.crop_nb_10000000
+./neighbors -a 1000 -R 3 -L 50 -b 1 -g $SG/100M_HCNNG_50 -q $SP/query.i8bin -c $SP/msspacev-100M -res $O/spacev_HCNNG.csv -f bin -t int8 $SP/spacev1b_base.i8bin.crop_nb_100000000
+./neighbors -a 1000 -R 3 -L 50 -b 1 -g $SG/1B_HCNNG_50 -q $SP/query.i8bin -c $SP/msspacev-1B -res $O/spacev_HCNNG.csv -f bin -t int8 $SP/spacev1b_base.i8bin
 
-# #TEXT2IMAGE: two settings
-TP=$P/text2image1B
-TG=$G/text2image1B
-./neighbors -a 1000 -R 3 -L 30 -b 1 -o $TG/1M_HCNNG_30 -q $TP/query.public.100K.fbin -c $TP/text2image-1M -res $TG/HCNNG_res.csv -f bin -t float -D 1 $TP/base.1B.fbin.crop_nb_1000000
-./neighbors -a 1000 -R 3 -L 30 -b 1 -o $TG/10M_HCNNG_30 -q $TP/query.public.100K.fbin -c $TP/text2image-10M -res $TG/HCNNG_res.csv -f bin -t float -D 1 $TP/base.1B.fbin.crop_nb_10000000
-./neighbors -a 1000 -R 3 -L 30 -b 1 -o $TG/100M_HCNNG_30 -q $TP/query.public.100K.fbin -c $TP/text2image-100M -res $TG/HCNNG_res.csv -f bin -t float -D 1 $TP/base.1B.fbin.crop_nb_100000000
-./neighbors -a 1000 -R 3 -L 30 -b 1 -o $TG/1B_HCNNG_30 -q $TP/query.public.100K.fbin -c $TP/text2image-1B -res $TG/HCNNG_res.csv -f bin -t float -D 1 $TP/base.1B.fbin
+# # #TEXT2IMAGE: two settings
+# TP=$P/text2image1B
+# TG=$G/text2image1B
+# ./neighbors -a 1000 -R 3 -L 30 -b 1 -o $TG/1M_HCNNG_30 -q $TP/query.public.100K.fbin -c $TP/text2image-1M -res $TG/HCNNG_res.csv -f bin -t float -D 1 $TP/base.1B.fbin.crop_nb_1000000
+# ./neighbors -a 1000 -R 3 -L 30 -b 1 -o $TG/10M_HCNNG_30 -q $TP/query.public.100K.fbin -c $TP/text2image-10M -res $TG/HCNNG_res.csv -f bin -t float -D 1 $TP/base.1B.fbin.crop_nb_10000000
+# ./neighbors -a 1000 -R 3 -L 30 -b 1 -o $TG/100M_HCNNG_30 -q $TP/query.public.100K.fbin -c $TP/text2image-100M -res $TG/HCNNG_res.csv -f bin -t float -D 1 $TP/base.1B.fbin.crop_nb_100000000
+# ./neighbors -a 1000 -R 3 -L 30 -b 1 -o $TG/1B_HCNNG_30 -q $TP/query.public.100K.fbin -c $TP/text2image-1B -res $TG/HCNNG_res.csv -f bin -t float -D 1 $TP/base.1B.fbin
