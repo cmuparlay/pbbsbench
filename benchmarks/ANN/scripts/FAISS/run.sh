97eb985b16c6fbfc273cbf04fb29bd170c1e33f3
Ubuntu <azureuser@ANS1.1tndt1sllxmubnyf32xqp4ovzc.bx.internal.cloudapp.net>
2023-03-30 15:24:50 +0000

    prep for submission

--- benchmarks/ANN/scripts/FAISS/run.sh
+++ benchmarks/ANN/scripts/FAISS/run.sh
@@ -1,8 +1,25 @@
+#!/bin/bash
 cd ~/big-ann-benchmarks
+rm results
+mkdir /ssd1/results/FAISSresults
+ln -s /ssd1/results/FAISSresults results
 P=~/pbbsbench/benchmarks/ANN/scripts/FAISS
-datasets=("bigann-10M" "ssnpp-10M" "msspacev-10M" "text2image-10M" "bigann-100M" "ssnpp-100M" "msspacev-100M" "text2image-100M" "bigann-1B" "ssnpp-1B" "msspacev-1B" "text2image-1B")
-for d in ${datasets[@]}; do
-  python3 run.py --definitions $P/ANN.yaml --algorithm faiss-t1 --dataset $d
-done
+R=/ssd1/results/FAISSresults
+
+nohup python3 run.py --definitions $P/ANN.yaml --algorithm faiss-t1 --dataset "bigann-10M" > $R/bigann-10M.log
+nohup python3 run.py --definitions $P/ANN.yaml --algorithm faiss-t1 --dataset "msspacev-10M" > $R/msspacev-10M.log
+nohup python3 run.py --definitions $P/ANN.yaml --algorithm faiss-t1 --dataset "text2image-10M" > $R/text2image-10M.log
+nohup python3 run.py --definitions $P/ANN.yaml --algorithm faiss-t1 --dataset "ssnpp-10M" > $R/ssnpp-10M.log
+
+nohup python3 run.py --definitions $P/ANN.yaml --algorithm faiss-t1 --dataset "bigann-100M" > $R/bigann-100M.log
+nohup python3 run.py --definitions $P/ANN.yaml --algorithm faiss-t1 --dataset "msspacev-100M"  > $R/msspacev-100M.log
+nohup python3 run.py --definitions $P/ANN.yaml --algorithm faiss-t1 --dataset "text2image-100M" > $R/text2image-100M.log
+nohup python3 run.py --definitions $P/ANN.yaml --algorithm faiss-t1 --dataset "ssnpp-100M" > $R/ssnpp-100M.log
+
+nohup python3 run.py --definitions $P/ANN.yaml --algorithm faiss-t1 --dataset "bigann-1B" > $R/bigann-1B.log
+nohup python3 run.py --definitions $P/ANN.yaml --algorithm faiss-t1 --dataset "msspacev-1B"  > $R/msspacev-1B.log
+nohup python3 run.py --definitions $P/ANN.yaml --algorithm faiss-t1 --dataset "text2image-1B" > $R/text2image-1B.log
+nohup python3 run.py --definitions $P/ANN.yaml --algorithm faiss-t1 --dataset "ssnpp-1B" > $R/ssnpp-1B.log
+
 sudo chmod -R 777 results/
-python data_export.py --output res.csv
+python data_export.py --output /ssd1/results/FAISS_res.csv
