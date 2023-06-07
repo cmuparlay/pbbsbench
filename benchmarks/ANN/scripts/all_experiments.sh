17da538ba69f9d3a95e4d72186838b42b6f32cfe
Ubuntu <azureuser@ANS1.1tndt1sllxmubnyf32xqp4ovzc.bx.internal.cloudapp.net>
2023-06-07 22:23:33 +0000

    updated scripts for resubmit

--- benchmarks/ANN/scripts/all_experiments.sh
+++ benchmarks/ANN/scripts/all_experiments.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 cd ~/pbbsbench/benchmarks/ANN/scripts
 #download datasets
-bash download_datasets.sh
+# bash download_datasets.sh
 #prepare groundtruth for 1M slices
-bash prepare_datasets.sh
+# bash prepare_datasets.sh
 #nearest neighbor search experiments
-bash vamana.sh
-bash hcnng.sh
-bash pynndescent.sh
+nohup bash vamana.sh >> run_log.out
+nohup bash hcnng.sh >> run_log.out
+# bash pynndescent.sh
 #range search experiments
-cd ~/pbbsbench/benchmarks/rangeSearch/scripts
-bash all_experiments.sh
+# cd ~/pbbsbench/benchmarks/rangeSearch/scripts
+# bash all_experiments.sh
 #all experiments with FAISS
-cd ~/pbbsbench/benchmarks/ANN/scripts
-bash run.sh
+# cd ~/pbbsbench/benchmarks/ANN/scripts
+# bash run.sh
 
 
 
--- benchmarks/ANN/scripts/vamana.sh
+++ benchmarks/ANN/scripts/vamana.sh
@@ -3,11 +3,11 @@ cd ~/pbbsbench/benchmarks/ANN/vamana
 make clean all
 
 P=/ssd1/data
-G=/ssd1/bigann
+G=/ssd1/results
 BP=$P/bigann
 BG=$G/bigann
-./neighbors -R 64 -L 128 -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
-./neighbors -R 64 -L 128 -o $BG/1M_vamana_64_128 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $G/bigann_vamana.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
+# ./neighbors -R 64 -L 128 -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
+# ./neighbors -R 64 -L 128 -o $BG/1M_vamana_64_128 -q $BP/query.public.10K.u8bin -c $BP/bigann-1M -res $G/bigann_vamana.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_1000000
 # ./neighbors -R 64 -L 128 -o $BG/10M_vamana_64_128 -q $BP/query.public.10K.u8bin -c $BP/bigann-10M -res $G/bigann_vamana.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_10000000
 # ./neighbors -R 64 -L 128 -o $BG/100M_vamana_64_128 -q $BP/query.public.10K.u8bin -c $BP/bigann-100M -res $G/bigann_vamana.csv -f bin -t uint8 $BP/base.1B.u8bin.crop_nb_100000000
 # ./neighbors -R 64 -L 128 -o $BG/1B_vamana_64_128 -q $BP/query.public.10K.u8bin -c $BP/bigann-1B -res $G/bigann_vamana.csv -f bin -t uint8 $BP/base.1B.u8bin
@@ -15,7 +15,7 @@ BG=$G/bigann
 
 SP=$P/MSSPACEV1B
 SG=$G/MSSPACEV1B
-./neighbors -R 64 -L 128 -o $SG/1M_vamana_64_128 -q $SP/query.i8bin -c $SP/msspacev-1M -res $G/spacev_vamana.csv -f bin -t int8 $SP/spacev1b_base.i8bin.crop_nb_1000000
+# ./neighbors -R 64 -L 128 -o $SG/1M_vamana_64_128 -q $SP/query.i8bin -c $SP/msspacev-1M -res $G/spacev_vamana.csv -f bin -t int8 $SP/spacev1b_base.i8bin.crop_nb_1000000
 # ./neighbors -R 64 -L 128 -o $SG/10M_vamana_64_128 -q $SP/query.i8bin -c $SP/msspacev-10M -res $G/spacev_vamana.csv -f bin -t int8 $SP/spacev1b_base.i8bin.crop_nb_10000000
 # ./neighbors -R 64 -L 128 -o $SG/100M_vamana_64_128 -q $SP/query.i8bin -c $SP/msspacev-100M -res $G/spacev_vamana.csv -f bin -t int8 $SP/spacev1b_base.i8bin.crop_nb_100000000
 # ./neighbors -R 64 -L 128 -o $SG/1B_vamana_64_128 -q $SP/query.i8bin -c $SP/msspacev-1B -res $G/spacev_vamana.csv -f bin -t int8 $SP/spacev1b_base.i8bin
@@ -23,8 +23,8 @@ SG=$G/MSSPACEV1B
 
 TP=$P/text2image1B
 TG=$G/text2image1B
-./neighbors -a .9 -R 64 -L 128 -o $TG/1M_vamana_64_128 -q $TP/query.public.100K.fbin -c $TP/text2image-1M -res $G/spacev_vamana.csv -f bin -t float -D 1 $TP/base.1B.fbin.crop_nb_1000000
+# ./neighbors -a .9 -R 64 -L 128 -o $TG/1M_vamana_64_128 -q $TP/query.public.100K.fbin -c $TP/text2image-1M -res $G/spacev_vamana.csv -f bin -t float -D 1 $TP/base.1B.fbin.crop_nb_1000000
 # ./neighbors -a .9 -R 64 -L 128 -o $TG/10M_vamana_64_128 -q $TP/query.public.100K.fbin -c $TP/text2image-10M -res $G/spacev_vamana.csv -f bin -t float -D 1 $TP/base.1B.fbin.crop_nb_10000000
-# ./neighbors -a .9 -R 64 -L 128 -o $TG/100M_vamana_64_128 -q $TP/query.public.100K.fbin -c $TP/text2image-100M -res $G/spacev_vamana.csv -f bin -t float -D 1 $TP/base.1B.fbin.crop_nb_100000000
-# ./neighbors -a .9 -R 64 -L 128 -o $TG/1B_vamana_64_128 -q $TP/query.public.100K.fbin -c $TP/text2image-1B -res $G/spacev_vamana.csv -f bin -t float -D 1 $TP/base.1B.fbin
+# ./neighbors -a .98 -R 64 -L 128 -o $TG/100M_vamana_64_128 -q $TP/query.public.100K.fbin -c $TP/text2image-100M -res $G/t2i_vamana_resubmit.csv -f bin -t float -D 1 $TP/base.1B.fbin.crop_nb_100000000
+./neighbors -a .98 -R 64 -L 128 -o $TG/1B_vamana_64_128 -q $TP/query.public.100K.fbin -c $TP/text2image-1B -res $G/t2i_vamana_resubmit.csv -f bin -t float -D 1 $TP/base.1B.fbin
 
