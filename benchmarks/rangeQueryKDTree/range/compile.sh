4e34806a588721a01587b9586e5ecf3639306409
Magdalen Dobson <magdalen@aware.aladdin.cs.cmu.edu>
2023-07-06 12:51:44 -0400

    range updates

--- benchmarks/rangeQueryKDTree/range/compile.sh
+++ benchmarks/rangeQueryKDTree/range/compile.sh
@@ -4,3 +4,8 @@ set -o xtrace
 
 # lock-based
 g++ -DHOMEGROWN -pthread -mcx16 -O3 -std=c++17 -DNDEBUG -I . -DNoHelp -DVersioned -DLazyStamp   -include range_bench.h -o range_bench ../bench/rangeTime.C -DHOMEGROWN -pthread -ldl -L/usr/local/lib -ljemalloc
+
+
+# lock-based, path copying
+
+g++ -DHOMEGROWN -pthread -mcx16 -O3 -std=c++17 -DNDEBUG -I . -DNoHelp -DVersioned -DPathCopy -DLazyStamp -include range_bench.h -o range_bench_path_copy ../bench/rangeTime.C -DHOMEGROWN -pthread -ldl -L/usr/local/lib -ljemalloc
