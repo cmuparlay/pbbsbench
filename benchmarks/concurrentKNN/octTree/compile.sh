a4efbfe11edc52e3fbf9e2c6c0a9ac2b74cfbb38
Hao Wei <yuanhao1@cs.cmu.edu>
2024-02-01 15:46:51 -0500

    updated compile script

--- benchmarks/concurrentKNN/octTree/compile.sh
+++ benchmarks/concurrentKNN/octTree/compile.sh
@@ -9,5 +9,5 @@ g++ -DHOMEGROWN -pthread -mcx16 -O3 -std=c++17 -DNDEBUG -I . -DNoHelp -DVersione
 
 g++ -DHOMEGROWN -pthread -mcx16 -O3 -std=c++17 -DNDEBUG -I . -DNoHelp -DVersioned -DPathCopy -DHWStamp -include neighbors_bench.h -o neighbors_bench_path_copy ../bench/neighborsTime.C -DHOMEGROWN -pthread -ldl -L/usr/local/lib -ljemalloc
 
-# lock-free (still need to fix something)
-# g++ -DHOMEGROWN -pthread -mcx16 -O3 -std=c++17 -DNDEBUG -I . -DVersioned -DLazyStamp   -include neighbors_bench.h -o neighbors_bench ../bench/neighborsTime.C -DHOMEGROWN -pthread -ldl -L/usr/local/lib -ljemalloc
+# lock-free
+g++ -DHOMEGROWN -pthread -mcx16 -O3 -std=c++17 -DNDEBUG -I . -DVersioned -DLazyStamp   -include neighbors_bench.h -o neighbors_bench ../bench/neighborsTime.C -DHOMEGROWN -pthread -ldl -L/usr/local/lib -ljemalloc
